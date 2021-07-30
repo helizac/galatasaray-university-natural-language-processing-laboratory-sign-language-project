153333b964f5497f5116a67f7e4119b883e8fd7b
Furkan Erdi <54884571+helizac@users.noreply.github.com>
2021-07-30 20:36:29 +0300

    Update video-to-frame.py

--- Video To Frame/video-to-frame.py
+++ Video To Frame/video-to-frame.py
@@ -6,46 +6,6 @@ from tqdm import tqdm
 
 import borderline as bl
 
-"""
-
-Required File Layout :
-
-    - gesture_folder
-        - a
-            - video1.mp4
-            - video2.mp4
-            ...
-        - b
-        ...
-        
-Output File Structure :
-
-    - target_folder
-        - a
-            - video1
-                - video1_frame_1.jpeg
-                - video1_frame_2.jpeg
-                ...
-            - video2
-            ...
-        - b
-        ...   
-        
-"""
-
-"""
-
-How the file works :
-
-    On Terminal :
-
-        python3 video-to-frame.py gesture_folder target_folder
-
-        # In case there is a problem with your library versions
-        python video-to-frame.py gesture_folder target_folder
-
-"""
-
 
 # Converts videos to jpeg format images
 def convert(gesture_folder, target_folder):
